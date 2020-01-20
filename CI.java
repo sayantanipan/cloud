48d6a59472f156caa28ccbc65be5c46b086e3be7
Pan <837032@cognizant.com>
2020-01-20 18:07:25 +0530

    blaaaaaaaablaaaaaaaaaaaa

--- CI.java
+++ CI.java
@@ -1,5 +1,5 @@
 class CI{  
     public static void main(String args[]){  
-     System.out.println("new project started");  
+     System.out.println("new project started added ok new");  
     }  
 }
